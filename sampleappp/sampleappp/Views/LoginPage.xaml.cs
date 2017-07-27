70fc1a58e3d967d3e1bb7a55b4090e145e79f0cc
ayushi tandon <aytandon@teksystems.com>
2017-07-27 17:06:38 +0530

    second commit

---
+++ sampleappp/sampleappp/Views/LoginPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace sampleappp
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
